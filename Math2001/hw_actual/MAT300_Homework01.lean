a65b0c5b5883ac5b53a45cc08c0f5f16f57950a2
aidanjacobson <survivalking4@gmail.com>
2026-01-15 12:00:44 -0700

    did 6,7,9

--- Math2001/hw_actual/MAT300_Homework01.lean
+++ Math2001/hw_actual/MAT300_Homework01.lean
@@ -3,16 +3,26 @@ import Library.Basic
 
 math2001_init
 example {x y : ℤ} (h1 : x - 3 * y = 5) (h2 : y = 3) : x = 14 :=
-  sorry
+  calc
+    x = x-3*y+3*y := by ring
+    _ = 5 + 3*3 := by rw[h1,h2]
+    _ = 14 := by ring
 
 example {p q : ℚ} (h1 : p - 2 * q = 1) (h2 : q = -1) : p = -1 :=
-  sorry
+  calc
+    p = p - 2*q + 2*q := by ring
+    _ = 1 + 2*-1 := by rw[h1,h2]
+    _ = -1 := by ring
 
 example {u v : ℚ} (h1 : 4 * u + v = 3) (h2 : v = 2) : u = 1 / 4 :=
   sorry
 
 example {u v : ℝ} (h1 : u + 1 = v) : u ^ 2 + 3 * u + 1 = v ^ 2 + v - 1 :=
-  sorry
+  calc
+    u^2 + 3*u + 1 = (u+1)^2 + u := by ring
+    _ = v^2 + u := by rw[h1]
+    _ = v^2 + (u + 1) - 1 := by ring
+    _ = v^2 + v - 1 := by rw[h1]
 
 example {t : ℚ} (ht : t ^ 2 - 4 = 0) :
     t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 = 10 * t + 2 :=
