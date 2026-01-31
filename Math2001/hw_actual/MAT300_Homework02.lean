a3e84d441c6fe76640bd5f26098ee7ae66b107e2
aidanjacobson <survivalking4@gmail.com>
2026-01-30 20:01:27 -0700

    solve remaining problems

--- Math2001/hw_actual/MAT300_Homework02.lean
+++ Math2001/hw_actual/MAT300_Homework02.lean
@@ -6,23 +6,28 @@ math2001_init
 
 
 --1
-example {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 := by calc
-  a + 2*b = a+2*b+a := by ring
-  _ ≥ a + 2*b + 3 := by rel[h1]
-sorry
+example {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 := by
+  calc
+    a + b = (a + 2*b + a) / 2 := by ring
+    _ ≥ (4 + a) / 2 := by rel[h2]
+    _ ≥ (4 + 3) / 2 := by rel[h1]
+    _ = 7 / 2 := by ring
+    _ ≥ 3 := by numbers
 
 
 --2
 example {n : ℤ} (hn : n ≥ 10) : n ^ 4 - 2 * n ^ 2 > 3 * n ^ 3 := by
-  have h: n^4 - 3*n^3 - 2*n^2 > 0 := by
+  have h1 : n ^ 2 - 3 * n - 2 > 0 := by
     calc
-      n^4 - 3*n^3 - 2*n^2 ≥ 10^4 - 3*10^3 - 2*10^2 := by rel[hn]
-      _ = 6800 := by numbers
+      n ^ 2 - 3 * n - 2 = n * n - 3 * n - 2 := by ring
+      _ ≥ 10 * n - 3 * n - 2 := by rel[hn]
+      _ = 7 * n - 2 := by ring
+      _ ≥ 7 * 10 - 2 := by rel[hn]
       _ > 0 := by numbers
-  addarith[h]
-
-
-
+  calc
+    n ^ 4 - 2 * n ^ 2 = n ^ 2 * (n ^ 2 - 3 * n - 2) + 3 * n ^ 3 := by ring
+    _ > n ^ 2 * 0 + 3 * n ^ 3 := by rel[h1]
+    _ = 3 * n ^ 3 := by ring
 
 
 --3
