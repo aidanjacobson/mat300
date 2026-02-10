4e391c1b858f67839f734c0b3c7691e47f735556
aidanjacobson <survivalking4@gmail.com>
2026-02-10 16:50:02 -0700

    finish hw4p1

--- Math2001/hw_actual/MAT300_HW04_part_I.lean
+++ Math2001/hw_actual/MAT300_HW04_part_I.lean
@@ -6,16 +6,49 @@ math2001_init
 
 --1
 example {s : ℚ} (h1 : 3 * s ≤ -15) (h2 : 2 * s ≥ -10) : s = -5 := by
-  sorry
+  apply le_antisymm
+  calc
+    s = (3*s)/3 := by ring
+    _ ≤ -15/3 := by rel[h1]
+    _ = -5 := by numbers
+  calc
+    s = (2*s)/2 := by ring
+    _ ≥ -10/2 := by rel[h2]
+    _ = -5 := by ring
 
 --2
 example : ∃ a b : ℕ, a ≠ 0 ∧ 2 ^ a = 5 * b + 1 := by
-  sorry
+  use 4, 3
+  constructor
+  · numbers
+  · numbers
 
 --3
 example {a b : ℝ} (h1 : a - 4 * b = 0) (h2 : b + 7 = 9) : a = 8 ∧ b = 2 := by
-  sorry
-  
+  constructor
+  · calc
+      a = a - 4*b + 4*b := by ring
+      _ = 0 + 4*b := by rw[h1]
+      _ = 4*(b+7-7) := by ring
+      _ = 4*(9-7) := by rw[h2]
+      _ = 8 := by numbers
+  · calc
+      b = b + 7 - 7 := by ring
+      _ = 9 - 7 := by rw[h2]
+      _ = 2 := by numbers
+
 --4
 example {m : ℤ} (he : ∃ a : ℤ, 4 * a = m + 7) : m ≠ 26 := by
-  sorry
+  obtain ⟨ a, ha ⟩ := he
+  have h2: m = 4 * a - 7 := by addarith[ha]
+  obtain h3|h3 := le_or_succ_le a 8
+  apply ne_of_lt
+  calc
+    m = 4*a - 7 := by rw[h2]
+    _ ≤ 4*8 - 7 := by rel[h3]
+    _ < 26 := by numbers
+  apply ne_of_gt
+  calc
+    m = 4*a - 7 := by rw[h2]
+    _ ≥ 4*9 - 7 := by rel[h3]
+    _ > 26 := by numbers
