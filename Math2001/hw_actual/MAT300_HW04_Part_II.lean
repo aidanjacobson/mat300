0dc921d657d17d0d0fa23aeaf310c73eaff921e2
aidanjacobson <survivalking4@gmail.com>
2026-02-10 17:01:18 -0700

    finish hw4p2

--- Math2001/hw_actual/MAT300_HW04_Part_II.lean
+++ Math2001/hw_actual/MAT300_HW04_Part_II.lean
@@ -8,8 +8,27 @@ open Int
 
 --5
 example {a b : ℤ} (ha : Odd a) (hb : Even b) : Even (a * b) := by
-  sorry
-
+  dsimp[Even]
+  obtain ⟨x, hx⟩ := ha
+  obtain ⟨y, hy⟩ := hb
+  use 2*x*y + y
+  calc
+    a*b = (2*x+1) * (2*y) := by rw[hx,hy]
+    _ = 4*x*y + 2*y := by ring
+    _ = 2 * (2*x*y + y) := by ring
 --6
 example (n : ℤ) : Odd (n ^ 2 - n + 1) := by
-  sorry
+  obtain heven|hodd := Int.even_or_odd n
+  dsimp[Odd]
+  dsimp[Even] at heven
+  obtain ⟨a,ha⟩ := heven
+  use 2*a^2 - a
+  calc
+    n^2 - n + 1 = (2*a)^2 - (2*a) + 1 := by rw[ha]
+    _ = 2*(2*a^2 - a) + 1 := by ring
+  obtain ⟨b,hb⟩ := hodd
+  use 2*b^2 + b
+  calc
+    n^2 - n + 1 = (2*b+1)^2 - (2*b+1) + 1 := by rw[hb]
+    _ = 4*b^2 + 2*b + 1 := by ring
+    _ = 2*(2*b^2 + b) + 1 := by ring
