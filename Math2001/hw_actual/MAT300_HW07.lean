0d399186a144dfd7dd9ccfcd9cfe51e0e18e9f24
aidanjacobson <survivalking4@gmail.com>
2026-03-01 19:24:29 -0700

    solve 3

--- Math2001/hw_actual/MAT300_HW07.lean
+++ Math2001/hw_actual/MAT300_HW07.lean
@@ -11,10 +11,18 @@ math2001_init
 /-! # HW 07 MAT 300 -/
 
 -- 1 (Hint: x * y - 4 * y + x - 4 = (y + 1) * (x - 4))
-example : ∃! x : ℝ, ∀ y, x * y - 4 * y + x - 4 = 0 := sorry
+example : ∃! x : ℝ, ∀ y, x * y - 4 * y + x - 4 = 0 := by
+  use 4
+  dsimp
+  constructor
+  · intro y
+    calc
+      4*y - 4*y + 4 - 4 = 0 := by ring
 
 --2 (Hint: study Example 4.3.3 from textbook)
-example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 + 4 * a + 4 = x) : x = 0 := sorry
+example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 + 4 * a + 4 = x) : x = 0 := by
+  obtain ⟨a,ha1,ha2⟩ := hx
+  sorry
 
 /-- Example 4.3.3 from Textbook
 example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 = x) : x = 0 := by
@@ -37,10 +45,34 @@ example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 = x) : x = 0 := by
 
 --3 (Hint: le_or_gt and cancel)
 example {x y : ℝ} (n : ℕ) (hx : 0 ≤ x) (hn : 0 < n) (h : y ^ n ≤ x ^ n) :
-    y ≤ x := sorry
+    y ≤ x := by
+      obtain hle|hgt := le_or_gt n 0
+      obtain hlt|heq := lt_or_eq_of_le hle
+      have h2: 0 < 0 := by calc
+        0 < n := by rel[hn]
+        _ < 0 := by rel[hlt]
+      apply ne_of_lt at h2
+      contradiction
+
+      have h2: 0 < 0 := by calc
+        0 < n := by rel[hn]
+        _ = 0 := by rw[heq]
+      apply ne_of_lt at h2
+      contradiction
+
+      cancel n at h
+
 
 --4 (Hint Use mod_cases)
-example (n : ℤ) (hn : n ^ 2 ≡ 4 [ZMOD 5]) : n ≡ 2 [ZMOD 5] ∨ n ≡ 3 [ZMOD 5] := sorry
+example (n : ℤ) (hn : n ^ 2 ≡ 4 [ZMOD 5]) : n ≡ 2 [ZMOD 5] ∨ n ≡ 3 [ZMOD 5] := by
+  mod_cases h: n % 5
+  · have h2: n^2 ≡ 0 [ZMOD 5] := by calc
+      n^2 = n*n := by ring
+      _ ≡ 0*0 [ZMOD 5] := by rel[h]
+      _ ≡ 0 [ZMOD 5] := by numbers
+    sorry
+
+
 
 
 #check better_prime_test
