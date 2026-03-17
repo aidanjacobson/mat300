5905d93a49d398b1a5e78cc5dd80406c1be0b2d6
aidanjacobson <survivalking4@gmail.com>
2026-03-16 18:17:51 -0700

    finish hw8

--- Math2001/hw_actual/MAT300_HW08.lean
+++ Math2001/hw_actual/MAT300_HW08.lean
@@ -15,7 +15,24 @@ math2001_init
 -- it states that a is not strictly less than a
 example {m k p T : ℕ} (h_m_k_eq_p : m * k = p ) (h_p_lt_T_sq : p < T ^ 2) :
   m < T ∨ ¬(k ≥ T) := by
-  sorry
+    obtain h1|h2 := Nat.lt_or_ge m T
+    · left
+      apply h1
+    · have h3: m*k ≥ T*k := by rel[h2]
+      right
+      rw [h_m_k_eq_p] at h3
+      have h4: T*T > T*k := by calc
+        T*T = T^2 := by ring
+        _ > p := by  rel[h_p_lt_T_sq]
+        _ ≥ T*k := by rel[h3]
+      cancel T at h4
+      intro h_neg
+      have h_kltk: k < k := by calc
+        k ≥ T := by rel[h_neg]
+        _ > k := by rel[h4]
+      apply Nat.lt_irrefl k at h_kltk
+      exact h_kltk
+
 
 -- 2 (Section 5.2)
 example (P Q : Prop) : (¬P → ¬Q) ↔ (Q → P) := by
@@ -47,8 +64,27 @@ example : Prime 23 := by
 
 -- 4 (Section 4.5)
 example {n : ℤ} (hn : n + 3 = 7) : ¬ (Int.Even n ∧ n ^ 2 = 10) := by
-  sorry
+  intro hbad
+  have hn4: n = 4 := by addarith[hn]
+  obtain ⟨h_even, h_nsq⟩ := hbad
+  rw[hn4] at h_nsq
+  numbers at h_nsq
 
 -- 5 (Section 5.1)
 example (P Q : Prop) : ¬ (P ∨ Q) ↔ ¬ P ∧ ¬ Q := by
-  sorry
+  constructor
+  · intro h1
+    constructor
+    · intro hp
+      apply h1
+      left
+      apply hp
+    · intro hq
+      apply h1
+      right
+      apply hq
+  · intro h1 h2
+    obtain ⟨h_negp,h_negq⟩ := h1
+    obtain h3|h4 := h2
+    · contradiction
+    · contradiction
